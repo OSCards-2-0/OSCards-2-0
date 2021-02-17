f407a24d8b7390e511e234101dd984bebad5a1eb
Jason Speare <jcspeare@gmail.com>
2021-02-17 12:56:35 -0800

    Added user collection through userSchema and userController

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 // creation of express instance
 const app = express();
 const PORT = 3000;
-const mongoURI = `${process.env.MONGO_URI}`;
+const mongoURI = 'mongodb+srv://coleredfearn:pebble37@cluster0.jo3mv.mongodb.net/<OSCards>?retryWrites=true&w=majority';
 
 // connect to instance of mongodb atlas
 mongoose
@@ -30,9 +30,11 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // instantiate router(s) for data calls
+const userRouter = require('./routes/userRoutes.js');
 const cardRouter = require('./routes/cardRoutes.js');
 const deckRouter = require('./routes/deckRoutes.js');
 
+app.use('/user', userRouter); 
 app.use('/card', cardRouter);
 app.use('/deck', deckRouter);
 
